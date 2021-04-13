0bb43baf55105c18f1657267e674f41f3e450302
Paulo <mfjpaulo@gmail.com>
2021-04-13 12:21:07 +0100

    Remove instances of jsx/tsx

--- jest.config.js
+++ jest.config.js
@@ -2,23 +2,14 @@
  * @type {jest.ProjectConfig}
  */
 module.exports = {
-    roots: [
-      '<rootDir>/test'
-    ],
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest'
+  roots: ["<rootDir>/test"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "js", "json", "node"],
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.test.json",
     },
-    moduleFileExtensions: [
-      'ts',
-      'tsx',
-      'js',
-      'jsx',
-      'json',
-      'node'
-    ],
-    globals: {
-      'ts-jest': {
-        tsConfig: 'tsconfig.test.json'
-      }
-    }
-  }
+  },
+};
