45f276d396659f7627dab2ea71bc5f5b35fef689
Paulo <mfjpaulo@gmail.com>
2021-04-14 22:08:25 +0100

    Implement isElementOnscreen

--- src/ts/reveal-on-scroll.ts
+++ src/ts/reveal-on-scroll.ts
@@ -15,16 +15,17 @@ export class RevealOnScroll {
     delayBetweenQueuedElements = 150,
     thresholdToRevealElement = 0.2
   ) {
-    // If not in browser, ignore
+    // If not in browser (SSR), ignore
     if (!IN_BROWSER) return;
 
     this._elements = this.getAllElementsToReveal();
-    this._delayBetweenQueuedElements = delayBetweenQueuedElements;
-    this._thresholdToRevealElement = thresholdToRevealElement;
 
     // If intersectionObserver isn't supported (IE), force show all
     if (!HAS_INTERSECTION_OBSERVER) this.revealAllElements();
     else {
+      this._delayBetweenQueuedElements = delayBetweenQueuedElements;
+      this._thresholdToRevealElement = thresholdToRevealElement;
+
       const observer = this.createIntersectionObserver();
       this._elements.forEach((element) => observer.observe(element));
     }
@@ -49,14 +50,11 @@ export class RevealOnScroll {
             // Get element
             const element = entry.target as HTMLElement;
 
-            // Find if element is already in queue
             const queued = this._queueToShow.includes(element);
-
-            // Find if element is already visible/hidden
-            const visible = element.classList.contains(VISIBLE_CLASS);
+            const alreadyVisible = element.classList.contains(VISIBLE_CLASS);
             const hidden = element.classList.contains(HIDDEN_CLASS);
 
-            if (queued || visible || hidden) return;
+            if (queued || alreadyVisible || hidden) return;
             else {
               // todo: tweak threshold
               // const elementHeight = element.getBoundingClientRect().height;
@@ -93,21 +91,38 @@ export class RevealOnScroll {
       // Get element to show
       const element = this._queueToShow[0];
 
-      setTimeout(() => {
-        // If item hasn't already been shown...
-        if (!element.classList.contains(VISIBLE_CLASS)) {
-          // Trigger reveal
-          element.classList.remove(ON_SCROLL_CLASS);
-          element.classList.add(VISIBLE_CLASS);
-
-          // Remove item from queue
-          this._queueToShow.shift();
-        }
-
-        // Show next element
-        this._canRevealNext = true;
-        this.revealQueued();
-      }, this._delayBetweenQueuedElements);
+      // If element is already offscreen, reveal (won't see stagger)
+      if (!this.isElementOnscreen) this.revealElement(element);
+      else {
+        setTimeout(() => {
+          this.revealElement(element);
+        }, this._delayBetweenQueuedElements);
+      }
     }
+  }
+
+  protected isElementOnscreen(element: HTMLElement) {
+    const elementRect = element.getBoundingClientRect();
+    const viewHeight = Math.max(
+      document.documentElement.clientHeight,
+      window.innerHeight
+    );
+    return !(elementRect.bottom < 0 || elementRect.top - viewHeight >= 0);
+  }
+
+  protected revealElement(element: HTMLElement) {
+    const alreadyVisible = !element.classList.contains(VISIBLE_CLASS);
+    if (!alreadyVisible) {
+      // Trigger reveal
+      element.classList.remove(ON_SCROLL_CLASS);
+      element.classList.add(VISIBLE_CLASS);
+
+      // Remove item from queue
+      this._queueToShow.shift();
+    }
+
+    // Show next element
+    this._canRevealNext = true;
+    this.revealQueued();
   }
 }
